54e3ebd9cc472d18f8230bcb2aa79a5508841e04
idand2 <idanday2@gmail.com>
2021-07-28 00:32:12 +0300

    starting Doc

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,7 +11,7 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-
+  //initiate a private heroService instace- now we can use the service in our getHeroes method. 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -17,6 +19,8 @@ export class DashboardComponent implements OnInit {
   }
 
   getHeroes(): void {
+    //using heroSerivce.getHeroes() to get a list of 4 heroes 2-5. 
+    //then using an anonymos fucntion- "=>", to save it to the heroes array already declared.
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,7 +11,12 @@ import { HttpHeaders } from '@angular/common/http';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  //calling @input decorator so that when a user types a value into the text box
+  //it will be saved to the hero variable of type Hero. will be used only 
+  //when the user types in his input. 
   @Input() hero?:Hero;
+  //initiating private route,location and heroService, so they could be used
+  //in our methods.  
   constructor(
     private activatedRoute: ActivatedRoute,
     private location: Location,
@@ -23,14 +28,23 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
+    //saves the 'id' param from the url, and calling the heroService- thanks to our constructor.
+    // save the return value as this.hero . the subscribe waiting for the Observable<Hero>, which is 
+    // the return value of heroService.getHero() method that send a GET request and waits for response.
     const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero)
   }
   goBack(): void {
+    // uses the Location library to navigate 1 step back in the browser. 
     this.location.back();
   }
   updateHero(): void {
+    // makes sure the hero has value then uses the heroService updatedHero() method.
+    // the subscribe waiting for the Observable<Hero>, which is the return value of heroService.getHero()
+    // method that send a GET request and waits for response.
+    // TO DO: change the if - at the moment even if there was no change, a put request will
+    // be sent to the server. 
     if(this.hero){
       var body = {
         "_id": this.hero._id,
